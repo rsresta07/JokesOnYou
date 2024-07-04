eb40e7bcbbc4983452eff610dce875cfb9d1628d
Rameshwor Shrestha <r.sresta07@gmail.com>
2024-07-04 08:10:25 +0545

    add connection detector

--- app/src/main/java/com/example/jokesonyou/ConnectionDetector.java
+++ app/src/main/java/com/example/jokesonyou/ConnectionDetector.java
@@ -1,4 +1,29 @@
 package com.example.jokesonyou;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+
 public class ConnectionDetector {
+
+    private Context _context;
+
+    public ConnectionDetector(Context context) {
+        this._context = context;
+    }
+
+    public boolean isConnectingToInternet() {
+        ConnectivityManager connectivity = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivity != null) {
+            Network network = connectivity.getActiveNetwork();
+            if (network != null) {
+                NetworkCapabilities capabilities = connectivity.getNetworkCapabilities(network);
+                return capabilities != null && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET);
+            }
+        }
+        return false;
+    }
 }
